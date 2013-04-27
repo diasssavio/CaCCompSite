9613986456b0c30a4a96288c7e439ef7d8f9dc1e
Sávio S. Dias <soares-go@MacBook-de-Savio-Soares.local>
2013-04-27 19:16:32 -0300

    [Created] Templates e Views

--- caccomp/content/views.py
+++ caccomp/content/views.py
@@ -1,4 +1,28 @@
 # -*- encoding: utf-8 -*-
 #!/usr/bin/env python2.7
 
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+
+from models import Keyword
+from forms import FormKeyword
+
 # Create your views here.
+
+def addKey( request ):
+	'''
+	Adiciona um novo elemento. Faz a validação e salva como\
+	pertencendo ao usuário em sessão
+	'''
+
+	if request.method == 'POST' :
+		form = FormKeyword( request.POST, request.FILES )
+		if form.is_valid():
+			form.save()
+			
+			return render_to_response( 'salvo.html', {} )
+	else:
+		form = FormKeyword()
+
+	return render_to_response( 'addkey.html', { 'form' : form }, 
+			context_instance = RequestContext( request ) )
