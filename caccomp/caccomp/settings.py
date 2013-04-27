f8ba69a5747fc51efc2b71436574655352aa2bd0
Sávio S. Dias <soares-go@MacBook-de-Savio-Soares.local>
2013-04-26 22:02:59 -0300

    [Updated] Configurações iniciais
    Foi realizado algumas configurações iniciais do projeto e criado a
    aplicação de postagem de conteúdo.

--- caccomp/caccomp/settings.py
+++ caccomp/caccomp/settings.py
@@ -1,23 +1,27 @@
+# -*- encoding: utf-8 -*-
+#!/usr/bin/env python2.7
+
 # Django settings for caccomp project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Sávio Dias', 'diasssavio@gmail.com'),
+    ('Pedro Reis', 'pedroreis@uft.edu.br'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'caccomp',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'USER': 'postgres',
+        'PASSWORD': 'savio',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
--- caccomp/caccomp/urls.py
+++ caccomp/caccomp/urls.py
@@ -1,3 +1,6 @@
+# -*- encoding: utf-8 -*-
+#!/usr/bin/env python2.7
+
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
